8598cc333871a7ae34ba55cbd97765a1a96704b8
Дмитрий Емельянов <dimamix1234@yandex.ru>
2019-02-28 22:48:23 +0300

    working_dejkstra

--- test_graph_a.cpp
+++ test_graph_a.cpp
@@ -10,17 +10,23 @@ using namespace std;
 
 const uint32_t INF = 4294967295;
 
-const uint32_t min(vector<int> shwm, vector<vector<int>> visited){
-	uint32_t min = shwm[visited[0][0]];
-	uint32_t min_index = visited[0][0];
+const uint32_t min(vector<uint32_t> shwm, vector<bool> visited, uint32_t start_vertex){
+	uint32_t min = INF;
+	uint32_t min_index = 0;
 	for (int i = 0; i < shwm.size(); ++i){
-		if ((shwm[i] < INF) and (visited[i][1] != true) )
+		if (i == start_vertex) continue;
+		// Ошибка здесь - visited становится короче, чем shwm
+		if ((shwm[i] <= min) and (visited[i] != true))
 		{
 			min = shwm[i];
 			min_index = i;
 		}
 	}
+	cout << "MIN_INDEX " << min_index << endl;
 	return min_index;
+
+	// надо найти ребро с минимальным весом
+
 }
 
 
@@ -124,35 +130,59 @@ void find_in_depth(vector<vector<int>> graph, int index){
 }
 
 
-vector<int> Dijkstras_alg(Graph graph, int start_vertex){
+bool not_visited(vector<bool> visited){
+	for (int i = 0; i < visited.size(); ++i){
+		if (visited[i] == false) return true;
+	}
+	return false;
+}
+
+
+vector<uint32_t> Dijkstras_alg(Graph graph, uint32_t start_vertex){
 	
 	int size = graph.size();
-
-	vector<int> shwm(size, INF);//shortest_way_matrix; 
+	cout << size << endl;
+	vector<uint32_t> shwm(size, INF);//shortest_way_matrix; 
 	shwm[start_vertex] = 0;
 	cout << shwm[start_vertex] << endl;
-	vector<vector<int>> visited(size, {0, 0});
+
 	for (int i = 0; i < size; ++i){
-		visited[i][0] = i;
+		cout << shwm[i] << endl;
 	}
-	visited[start_vertex][1] = true;
-	cout << "hello world" << endl;
+	for (int i = 0; i < graph[start_vertex].size(); ++i){
+		if (graph[start_vertex][i] != 0){
+			shwm[i] = graph[start_vertex][i];
+		}
+	}
+	cout << "*************" << endl;
+	vector<bool> visited(size, 0);
+	// for (int i = 0; i < size; ++i){
+	// 	visited[i][0] = i;
+	// }
+	visited[start_vertex] = true;
+	
  	// for (int i = 0; i < size; ++i){
 		// for (int j = 0; j < size; ++j){
 			// shwm[i*size + j] = INF;
 		// }
 	// }
 	int cur_v = 0;
-	while (visited.size()){
-		cur_v = min(shwm, visited);
+	while (not_visited(visited)){
+
+		cur_v = min(shwm, visited, start_vertex);
+		cout << cur_v << endl;
 		for (int i = 0; i < size; ++i){
-			if (shwm[i] > shwm[cur_v] + graph[cur_v][i]){
+			if (shwm[i] > shwm[cur_v] + graph[cur_v][i] and graph[cur_v][i] != 0){
+				// cout << "hello world" << endl;
 				shwm[i] = shwm[cur_v] + graph[cur_v][i];
+				cout <<"Shwm " << i << " "  << shwm[i] << endl;
 			}
 			// cur_v = 
 		}
-		visited.erase(visited.begin()+cur_v);
+		// visited.erase(visited.begin()+cur_v);
+		visited[cur_v] = true;
 	}
+	cout << "SIZE BEFORE RETURN: " << shwm.size() << endl;
 	return shwm;
 	// for (int i = 0; i < graph[0].size(); ++i){
 	// 	vector<int> v;
@@ -164,8 +194,8 @@ vector<int> Dijkstras_alg(Graph graph, int start_vertex){
 
 
 int main(){
-	vector<vector<int>> graph = {{0, 1, 1, 0, 0, 0, 0},
-						 {1, 0, 0, 1, 1, 0, 0},
+	vector<vector<int>> graph = {{0, 2, 1, 0, 0, 0, 0},
+						 {3, 0, 0, 1, 1, 0, 0},
 						 {1, 0, 0, 1, 0, 0, 1},
 						 {0, 1, 1, 0, 1, 1, 0},
 						 {0, 1, 0, 1, 0, 0, 0},
@@ -184,12 +214,12 @@ int main(){
 	// cout <<  (NULL > 12) << endl;
 
 	Graph obj(graph);
-	auto v = Dijkstras_alg(graph, 0);
+	auto v = Dijkstras_alg(obj, 0);
 	for (int i = 0; i < v.size(); ++i){
 		cout << v[i] << endl;
 	}
 
-	obj.print_graph();
+	// obj.print_graph();
 	cout << "EXIT" << endl;
 
 	return 0;
