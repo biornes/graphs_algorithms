921e5a30926b2ab193b0801d2f6c7f1784876959
Дмитрий Емельянов <dimamix1234@yandex.ru>
2019-03-05 23:00:29 +0300

    in progress 05.03.2019 23.00

--- B/graph_b.cpp
+++ B/graph_b.cpp
@@ -3,4 +3,87 @@
 
 using namespace std;
 
+
+template <typename T>
+struct adjacent_vertex
+{
+	size_t num;
+	T length;
+};
+
+template <typename T>
+class Graph
+{
+	private:
+		vector<vector<T>> graph_;
+	public:
+	Graph(vector<vector<T>> &graph): graph_(graph)
+	{
+
+	}
+	size_t size()
+	{
+		return graph_.size();
+	}
+
+	void print_graph()
+	{
+		for (int i = 0; i < graph_.size(); ++i)
+		{
+			for (int j = 0; j < graph_[i].size(); ++j)
+			{
+				cout << graph_[i][j];
+			}
+			cout << endl;
+		}
+	}
+	vector<T>& operator[](uint32_t index)
+	{
+		return graph_[index];
+	}
+	void optimize()
+	{
+		size_t size = graph_.size();
+		// временный массив для хранения оптимизированного вектора
+		vector<vector<T>> temp(size, {});
+		for (int i = 0; i < size; ++i)
+		{
+			for (int j = 0; j < size; ++j)
+			{
+				if (graph_[i][j] != 0){
+					temp[i].push_back()
+				}
+			}
+		}
+		// graph_
+	}
+};
+
+
+
+
+template <typename T>
+const auto Tarjans_algorithm(const Graph<T> &graph)
+{
+	// массив посещенных вершин
+	size_t size = graph.size();
+	vector<bool> visited(size, false);
+	for (int i = 0; i < size; ++i){
+		if (visited[i] == false)
+		{
+			// dfs_inv(i);
+		}
+	}
+}
+
+template <typename T>
+const auto dfs_inv(vector<size_t> &stack, vector<bool> &visited, size_t vertex, const Graph<T> &graph)
+{
+	size_t size = graph[vertex].size();
+	visited[vertex] = true;
+	for (int i = 0; i < size; ++i){
+		// if 
+	}
+}
+
 #endif
--- B/graph_b.h
+++ B/graph_b.h
@@ -4,7 +4,9 @@ using namespace std;
 template <typename T>
 class Graph;
 template <typename T>
-const uint32_t Tarjans_algorithm ();
+const uint32_t Tarjans_algorithm (const Graph<T> &graph);
+template <typename T>
+auto dfs_inv(vector<size_t> &stack, vector<bool> &visited, size_t vertex, const Graph<T> &graph);
 // template <typename T>
 // bool find_in_stack(T stack, int vertex);
 
